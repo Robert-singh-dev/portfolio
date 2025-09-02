d198afff125433a5fbef06bf9da73fa67ea7c917
Robert Singh <robert.singh@supercode.in>
2025-09-03 00:42:40 +0530

    Initial set up portfolio

---
+++ src/app/home/components/home/Header.jsx
@@ -0,0 +1,21 @@
+export default function Hero() {
+  return (
+    <>
+      <header>
+        <div className="logoContainer">
+          <a href="#">Logo</a>
+        </div>
+        <nav className="navList">
+          <a href="#">About</a>
+          <a href="#">Skills</a>
+          <a href="#">Projects</a>
+        </nav>
+        <div className="socialLinksHeader">
+          <a href="#">E</a>
+          <a href="#">G</a>
+          <a href="#">L</a>
+        </div>
+      </header>
+    </>
+  );
+}
---
+++ src/app/home/components/home/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1 className="heading-1">Hi, I’m Robert</h1>
+      <h2 className="heading-2">Frontend Developer</h2>
+      <p className="text-1">
+        I build modern, interactive, and aesthetic web experiences.
+      </p>
+    </section>
+  );
+}
